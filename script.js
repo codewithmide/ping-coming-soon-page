a75c57f6c82e24dc6ace50a7fe739b71004f4e58
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-29 16:25:54 +0100

    the work is done

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
-/*const form = document.getElementById('form');
-
-form.addEventListener('submit', e => {
-	e.preventDefault();
-
-	const email = form['email'].value;
-
-	if (email === '') {
-		addErrorTo('email', 'Email is required');
-	} else if (!isValid(email)) {
-		addErrorTo('email', 'Email is not valid');
-	} else {
-		removeErrorFrom('email');
-	}
-});
-
-function addErrorTo(field, message) {
-	const formControl = form[field].parentNode;
-	formControl.classList.add('error');
-
-	const small = formControl.querySelector('small');
-	small.innerText = message;
-}
-
-function removeErrorFrom(field) {
-	const formControl = form[field].parentNode;
-	formControl.classList.remove('error');
-}
-
-function isValid(email) {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
-}*/
-
 const form = document.getElementById("form");
 const email = document.getElementById("email");
+const btn = document.getElementById("btn");
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const emailVal = email.value;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!validateEmail(emailVal)) {
-        form.classList.add("error");
-    } else {
+    if (email.value.match(re)) {
         form.classList.remove("error");
+    } else {
+        form.classList.add("error");
     }
 });
-
-function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
